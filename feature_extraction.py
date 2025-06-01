28fbe0aa814962359b255b5075ff8e239275df47
ADAALMAN <Adaalman@outlook.com>
2025-06-01 21:58:23 +0200

    added BW w/o micro doppler

--- feature_extraction.py
+++ feature_extraction.py
@@ -15,9 +15,10 @@ def feature_extraction(spectrogram, f, t):
 
     Features include:
     - 'normalized_std': Variance of the spectrogram, normalized by the mean.
-    - 'period': Period of the spectrogram, calculated as the mean of the time differences between peaks in the high frequency envelope (positive mirco Doppler).
-    - 'offset': Offset of the spectrogram, calculated as the difference between the mean (across time) of the high and low frequency envelopes (highest and lowest micro Doppler frequencies across time).
-    - 'bandwidth': Bandwidth of the spectrogram, calculated as the difference between the maximum and minimum micro Doppler frequencies.
+    - 'period': Period of the signature, calculated as the mean of the time differences between peaks in the high frequency envelope (positive mirco Doppler).
+    - 'offset': Offset of the signature, calculated as the difference between the mean (across time) of the high and low frequency envelopes (highest and lowest micro Doppler frequencies across time).
+    - 'bandwidth': Bandwidth of the signature, calculated as the difference between the maximum and minimum micro Doppler frequencies.
+    - 'bandwidth_wo_uD': Bandwidth of the signature without the micro Doppler, calculated as the difference between the minimum and maximum micro Doppler frequencies.
     - 'torso frequency':  The average frequency of the peak signal in strength over the time bins within the window.
     """
     #threshold_db = -10 # set threshold to -10 dB to filter out noise
@@ -40,6 +41,7 @@ def feature_extraction(spectrogram, f, t):
         'period': np.mean(np.diff(peak_times)),
         'offset': np.mean(envelope_high) - np.mean(envelope_low), 
         'bandwidth': np.max(envelope_high) - np.min(envelope_low),
+        'bandwidth_wo_uD': np.min(envelope_high) - np.max(envelope_low),
         'torso frequency': np.mean(peak_freqs)
     }
     
